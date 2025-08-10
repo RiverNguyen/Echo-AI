43a44b36d759c65d35e753d2c0492be34913513e
JangCook <ngiang1309@gmail.com>
2025-08-10 12:29:11 +0700

    [update]: enhance widget and web apps with Convex integration, add Next.js TopLoader, and update dependencies

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -7,9 +7,11 @@ export default function Page() {
   const users = useQuery(api.users.getMany);
   console.log(users);
   return (
-    <div className="flex items-center justify-center min-h-svh">
+    <div className="flex flex-col items-center justify-center min-h-svh">
       <p>web</p>
-      {JSON.stringify(users)}
+      <div className="max-w-sm w-full mx-auto">
+        {JSON.stringify(users, null, 2)}
+      </div>
     </div>
   );
 }
